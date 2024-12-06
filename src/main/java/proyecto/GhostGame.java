1f62e058faf230e03ce4c354f199999c49d8c2c6
carlo <carlo@PCdeCarlosM>
2024-12-05 19:58:49 -0600

    GHOSTS#3

--- src/main/java/proyecto/GhostGame.java
+++ src/main/java/proyecto/GhostGame.java
@@ -29,10 +29,10 @@ public class GhostGame {
     public void iniciarTablero(){
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
-                tablero[i][j] ='E';
+                tablero[i][j] ='#';
             }
         }
-        tablero[0][0] = tablero[0][5] = tablero[5][0] = tablero[5][5] = 'B';
+        tablero[0][0] = tablero[0][5] = tablero[5][0] = tablero[5][5] = 'S';
     }
     
     public boolean registrarPlayer(String username, String password){
@@ -73,14 +73,20 @@ public class GhostGame {
         switch(nivelDificultad){
             case 1:
                 dificultad=8;
+                System.out.println("Dificultad NORMAL seleccionada:  8 fantasmas por jugador (4 buenos y 4 malos)");
                 break;
             case 2:
                 dificultad=4;
+                System.out.println("Dificultad EXPERT seleccionada: 4 fantasmas por jugador (2 buenos y 2 malos)");
                 break;
             case 3:
                 dificultad=2;
+                System.out.println("Dificultad GENIUS seleccionada: 2 fantasmas buenos y 4 trampas por jugador");
                 break;
+            default:
+                System.out.println("OPCION INVALIDA");
         }
+        System.out.println("LA DIFICULTADA SE HA ACTUALIZADO");
     }
     
     public void configurarMode(String nuevoMode){
@@ -100,13 +106,13 @@ public class GhostGame {
         for (int i = 0; i < dificultad; i++) {
             boolean esBueno = i < ghostBueno;
             
-            char piece = esBueno ? 'G' : 'R';
+            char piece = esBueno ? 'G' : 'M';
             
             while(true){
                 int fila=playerIndex== 0 ? random.nextInt(2) : 4 + random.nextInt(2);
                 int columna=random.nextInt(6);
                 
-                if(tablero[fila][columna] == 'E'){
+                if(tablero[fila][columna] == '#'){
                     tablero[fila][columna] =piece;
                     break;
                 }
@@ -115,14 +121,17 @@ public class GhostGame {
     }
     
     public void ponerPiezasManual(int playerIndex){
+        
         int contarBueno= dificultad/2;
         int contarMalo= dificultad/2;
         
         for (int i = 0; i < dificultad; i++) {
-            char piece=(i < contarBueno) ? 'G' : 'R';
+            char piece=(i < contarBueno) ? 'G' : 'M';
+            boolean validar=false;
             
-            while(true){
-                System.out.println("Jugador"+(playerIndex+1)+", coloca un fantasma"+(piece=='G' ? "bueno" : "malo")+":");
+            while(!validar){
+                System.out.println("Jugador "+(playerIndex+1)+", coloca un fantasma"+(piece=='G' ? "bueno" : "malo")+":");
+                
                 System.out.println("Fila: ");
                 int fila=lea.nextInt();
                 
@@ -131,7 +140,9 @@ public class GhostGame {
                 
                 if (validarMovimiento(fila,columna,playerIndex)) {
                     tablero[fila][columna]=piece;
-                    break;
+                    System.out.println("Fantasma colocado en (" + fila+ ", " + columna+ ").");
+
+                    validar=true;
                 }else{
                     System.out.println("MOVIMIENTO INVALIDO | INTENTA OTRA POSISCION");
                 }
@@ -140,22 +151,23 @@ public class GhostGame {
     }
     
     private boolean validarMovimiento(int fila, int columna, int playerIndex){
+        if (columna < 0 || columna>=6) 
+            return false;
         if (playerIndex==0 && (fila < 0 || fila >=2)) 
             return false;
         if (playerIndex==1 && (fila < 4 || fila>=6)) 
             return false;
-        if (columna < 0 || columna >= 6 || tablero[fila][columna] !='E')
-            return false;
-        return true;
+            
+        return tablero[fila][columna] == '#';
     }
     
 
 
     
     public void mostrarTablero(){
-        for (char[] fila : tablero) {
-            for (char celda : fila) {
-                System.out.print(celda+" ");
+        for (int i=0;i<tablero.length; i++) {
+            for (int j =0; j<tablero[i].length; j++) {
+                System.out.print(tablero[i][j] + " ");
             }
             System.out.println();
         }
@@ -163,7 +175,81 @@ public class GhostGame {
     
     public void empezarJuego(){
         System.out.println("INICIANDO.....");
-        mostrarTablero();
+        if (mode.equalsIgnoreCase("MANUAL")) {
+            System.out.println("Modo manual seleccionado");
+            ponerPiezasManual(0);
+            ponerPiezasManual(1);
+        }else{
+            System.out.println("Modo aleatorio seleccionado");
+            ponerPiezasRandom(0);
+            ponerPiezasRandom(1);
+        }
+        
+        System.out.println("LISTO COMIENZEN !!!!!!!");
+        
+        int turno=0;
+        boolean jugando=true;
+        
+        while(jugando){
+            mostrarTablero();
+            System.out.println("Turno del jugador "+(turno+1));
+            System.out.println("Ingrese fila y la columna del fantasma que desea mover");
+            System.out.println("Fila: ");
+            int filaOG=lea.nextInt();
+            System.out.println("Columna: ");
+            int columnaOG=lea.nextInt();
+            
+            if(!validarSeleccion(filaOG, columnaOG, turno)){
+                System.out.println("Seleccion invalida. Intentelo de nuevo");
+                continue;
+            }
+            
+            System.out.println("Ingrese la fila y la columna del movimiento: ");
+            System.out.println("Fila: ");
+            int filaMover=lea.nextInt();
+            System.out.println("Columna: ");
+            int columnaMover=lea.nextInt();
+            
+            if (validarMovimiento(filaMover, columnaMover, turno) && tablero[filaOG][columnaOG] != '#') {
+                tablero[filaMover][columnaMover]=tablero[filaOG][columnaOG];
+                tablero[filaOG][columnaOG]='#';
+                System.out.println("MOVIMIENTO ECHO");
+                mostrarTablero();
+            }else{
+                System.out.println("MOVIMIENTO INVALIDO | INTENTE OTRAS VEZ");
+                mostrarTablero();
+                continue;
+            }
+            if (validarVictoria(turno)) {
+                System.out.println("PLAYER "+(turno+1)+" ha ganado!!!!");
+                jugando=false;
+            }else{
+                 turno=1-turno;
+            }
+           
+        }
+    }
+    
+    private boolean validarSeleccion(int fila, int columna, int turno){
+        if (fila<0 || fila>=6 || columna < 0 || columna >=6) 
+            return false;
+        char pieza=tablero[fila][columna];
+        return pieza =='G' || pieza == 'M';
+    }
+    
+    private boolean validarVictoria(int turno){
+        int buenos=0, malos=0;
+        for (char[] fila : tablero) {
+            for (char cell : fila) {
+                if (cell == 'G') 
+                    buenos++;
+                if (cell == 'M') 
+                    malos++;
+            }
+        }
+        if(buenos==0 || malos==0)
+            return true;
+        return tablero[0][0]=='G' || tablero[0][5]=='G' || tablero[5][0]=='G' || tablero[5][5]=='G';
     }
     
     public void eliminarUser(){
